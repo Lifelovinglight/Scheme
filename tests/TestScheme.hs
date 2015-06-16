9aa963bb16ce72ecf34be1a0d54325cf35884924
Victor Fors <krakow89@gmail.com>
2015-06-16 19:17:42 +0200

    This seems to work.

--- tests/TestScheme.hs
+++ tests/TestScheme.hs
@@ -12,10 +12,27 @@ import Control.Monad.State
 
 main :: IO ()
 main = defaultMainWithOpts
-       [ testCase "car" testCar
+       [ testCase "car" testCar,
+         testCase "cdr" testCdr,
+         testCase "primitiveCons" testPrimitiveCons
        ] mempty
 
 testCar :: Assertion
 testCar = do
   let (r, env) = runState (car (HeapPointer 1) >>= dereference) $ SchemeEnvironment (Heap (fromList [(HeapPointer 0, SchemeInteger 1), (HeapPointer 1, SchemeCons (HeapPointer 0) SchemeNil)]) (HeapPointer 2)) SchemeNil SchemeNil
   r @?= SchemeInteger 1
+  
+testCdr :: Assertion
+testCdr = do
+  let (r, env) = runState (cdr (HeapPointer 1) >>= dereference) $ SchemeEnvironment (Heap (fromList [(HeapPointer 0, SchemeInteger 1), (HeapPointer 1, SchemeCons SchemeNil (HeapPointer 0))]) (HeapPointer 2)) SchemeNil SchemeNil
+  r @?= SchemeInteger 1
+  
+testPrimitiveCons :: Assertion
+testPrimitiveCons = do
+  let (r, (SchemeEnvironment (Heap heap index) env cont)) = runState (primitiveCons (HeapPointer 0) (HeapPointer 1)) $ SchemeEnvironment (Heap (fromList [(HeapPointer 0, SchemeInteger 1), (HeapPointer 1, SchemeInteger 2)]) (HeapPointer 2)) SchemeNil SchemeNil
+  r @?= HeapPointer 2
+  index @?= HeapPointer 3
+  (toList heap) @?= [(HeapPointer 0, SchemeInteger 1), (HeapPointer 1, SchemeInteger 2), (HeapPointer 2, SchemeCons (HeapPointer 0) (HeapPointer 1))]
+
+  
+  
